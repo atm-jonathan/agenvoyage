9dbe20ce9ab42e3dd2405e130e1659e0a9ac1b35
Florian Mortgat <50440633+atm-florianm@users.noreply.github.com>
2022-02-09 10:49:34 +0100

    Update chiffrage_card.php

--- chiffrage_card.php
+++ chiffrage_card.php
@@ -206,7 +206,7 @@ if (empty($reshook)) {
 
 
     if ($action == 'create') {
-        $object->fields['po_estimate']['default'] = $user->id;
+		$object->fields['po_estimate']['default'] = $user->id;
 		$object->fields['fk_product']['default'] = $conf->global->CHIDefaultProduct;
 		$object->fields['tech_detail']['visible'] = 5;
     }
