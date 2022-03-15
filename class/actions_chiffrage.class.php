54a86e947a277ab7c6555e975c535ebe396bf892
GregM <gregory.maza@atm-consulting.fr>
2022-03-15 11:09:37 +0100

    add urlencode on chiffrageCreate button since Ticket

--- class/actions_chiffrage.class.php
+++ class/actions_chiffrage.class.php
@@ -194,7 +194,8 @@ class ActionsChiffrage
 
 		$context = explode(':', $parameters['context']);
 		if (in_array('ticketcard', $context)) {
-			print '<a class="butAction" href="'.dol_buildpath('/chiffrage/chiffrage_card.php?action=create', 1). '&fk_soc='.$object->fk_soc.'&module_name='.$object->array_options['options_fk_webmodule'].'&commercial_text='.'Demande_'.$object->ref.'&fk_ticket='.$object->id.'">'.$langs->trans("NewChiffrage").'</a>';
+			$descriptionTicketChiffrage = $langs->trans('ChiffrageAskTicketTitle', $object->ref);
+			print '<a class="butAction" href="'.dol_buildpath('/chiffrage/chiffrage_card.php?action=create', 1). '&fk_soc='.$object->fk_soc.'&module_name='.$object->array_options['options_fk_webmodule'].'&commercial_text='.urlencode($descriptionTicketChiffrage).'&fk_ticket='.$object->id.'">'.$langs->trans("NewChiffrage").'</a>';
 		}
 
 		return 0;
