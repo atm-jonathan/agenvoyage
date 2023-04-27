0a43409fa1279f8bed7af4b1468006066ae93684
jpb <jean-pascal.boudet@atm-consulting>
2023-04-27 10:57:59 +0200

    .

--- class/chiffrage.class.php
+++ class/chiffrage.class.php
@@ -222,6 +222,7 @@ class Chiffrage extends CommonObject
 		}
 
 		if ( version_compare(DOL_VERSION,'17.0.0') > 0 ) {
+
 			$this->fields['po_estimate']['type'] = 'integer:User:user/class/user.class.php:1:((employee:=:1) AND (statut:=:1))';
 			$this->fields['dev_estimate']['type'] = 'integer:User:user/class/user.class.php:1:((employee:=:1) AND (statut:=:1))';
 			$this->fields['fk_soc']['type'] = 'integer:Societe:societe/class/societe.class.php:1:(status:=:1)';
